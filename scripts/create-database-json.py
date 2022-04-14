2d72b3f5a0c3a4a370e81f15139b7e4635dc9dd4
othmar52 <othmar52@users.noreply.github.com>
2022-04-14 09:06:34 +0200

    [TASK] drop eyed3 in favour of ffprobe

--- scripts/create-database-json.py
+++ scripts/create-database-json.py
@@ -1,27 +1,21 @@
 #!/usr/bin/python3
 
 # install http://www.pogo.org.uk/~mark/bpm-tools/ for bpm detection
-# sudo pip install eyed3  
+# install ffprobe for reading music file metadata
+
 
 # TODO make bpm detect and keyfinder optional
 
 # TODO read music directory from .env file(s) and add environment argument
 
-
-import logging
-from shutil import copyfile
-import subprocess
-import time
 import json
 import hashlib
 import sys
 import re
 import os
-import select
 import glob
-from subprocess import Popen, PIPE, DEVNULL, STDOUT
+from subprocess import Popen, PIPE
 from pathlib import Path
-import eyed3
 
 import argparse
 
@@ -30,11 +24,25 @@ import argparse
 # include trailing slash!
 targetJson = "instrupella-db.json"
 mergePropsJson = "trackprops*.json"
-musicDir = "/MUSIC/aca2/testaca/"
+
+musicDir = "/MUSIC/testaca/"
 pathSearchReplace = [
-    "/MUSIC/aca2/testaca/",
+    "/MUSIC/testaca/",
     "./testaca/"
 ]
+
+musicDir = "/MUSIC/aca2/2022-04/kingpella/"
+pathSearchReplace = [
+    "/MUSIC/aca2/2022-04/kingpella/",
+    "./aca2/2022-04/kingpella/"
+]
+
+musicDir = "/MUSIC/aca2/2022-02/"
+pathSearchReplace = [
+    "/MUSIC/aca2/2022-02/",
+    "./aca2/2022-02/"
+]
+
 
 musicDir = "/MUSIC/aca2/"
 pathSearchReplace = [
@@ -43,6 +51,7 @@ pathSearchReplace = [
 ]
 
 
+createPeakFile = False
 
 mergeProps = {}
 
@@ -190,6 +199,18 @@ def detectKey(filePath):
     return  p1.communicate()[0].decode("utf-8").strip()
 
 
+def readTags(filePath):
+    p1 = Popen(["/usr/bin/ffprobe", "-v", "quiet", "-print_format", "json=compact=1", "-show_format", filePath], stdout=PIPE, stderr=PIPE)
+    obj = json.loads(p1.communicate()[0].decode("utf-8"))
+    if 'format' in obj:
+        return obj['format']
+
+    try:
+        fallback = {"size": int(os.path.getsize(filePath))}
+    except os.error:
+        fallback = {"size": 0}
+
+    return fallback
 
 
 def writeTextFile(fileNameToWrite, fileContent):
@@ -200,10 +221,37 @@ def collectAllMusicFiles(startDirectory):
     allFiles = sorted(Path(startDirectory).rglob('*.*'))
     musicFiles = []
     for foundFile in allFiles:
-        if foundFile.suffix.lower() in ['.wav', '.flac', '.mp3', '.mp4', '.ogg', '.oga']:
+        if foundFile.suffix.lower() in ['.wav', '.flac', '.mp3', '.mp4', '.ogg', '.oga', '.m4a']:
             musicFiles += [ foundFile ]
             continue
     return musicFiles
+
+def getTagsForFile(dbItem):
+    tags = readTags(dbItem["path"])
+    try:
+        tags_lower = {k.lower():v for k,v in tags["tags"].items()}
+    except KeyError:
+        tags_lower = {
+            "artist": Path(dbItem["path"]).stem,
+            "title": Path(dbItem["path"]).stem,
+            "year": 0
+        }
+    #print(tags_lower)
+    dbItem["size"] = int(tags["size"])
+    dbItem["length"] = float(tags["duration"])
+    for k in ["artist", "title", "year"]:
+        if k in tags_lower:
+            dbItem[k] = tags_lower[k]
+    if "date" in tags_lower:
+        dbItem["year"] = tags_lower["date"]
+    try:
+        # ValueError: could not convert string to float: '2014-08-22'
+        dbItem["year"] = int(float(dbItem["year"]))
+    except ValueError:
+        pass
+    #print(tags)
+    return dbItem
+
 
 def createDatabaseJson():
     files = collectAllMusicFiles(musicDir)
@@ -213,62 +261,42 @@ def createDatabaseJson():
     print("found music files", len(files))
     for file in files:
         path = str(file)
-        audiofile = eyed3.load(file)
-        if audiofile == None:
-            continue
-            print("ERROR:", file)
-            continue
-        artist = Path(file).stem
-        title = Path(file).stem
-        album = ""
-        year = 0
-        length = 0
-        size = 0
-        if len(pathSearchReplace) == 2:
-            path = path.replace(pathSearchReplace[0], pathSearchReplace[1])
-        if audiofile.tag != None:
-            artist = audiofile.tag.artist if audiofile.tag.artist != "" else artist
-            title = audiofile.tag.title if audiofile.tag.title != "" else title
-            album = audiofile.tag.album
-            tmpYear = audiofile.tag.getBestDate()
-            if tmpYear != None:
-                year = int(tmpYear.year)
-        if audiofile.info != None:
-            length = float("{:.3f}".format(audiofile.info.time_secs))
-
-        try:
-            size = int(os.path.getsize(file))
-        except os.error:
-            size = 0
-
-        md5 = hashlib.md5(path.encode()).hexdigest()
-        peakJsonFileName = Path(path).stem[0:30]
-        peakJsonFilePath = f'{peakfilesDir}/{peakJsonFileName}-{md5}.json'
-        createPeakFile(file, peakJsonFilePath)
-        #sys.exit()
-
-        jsonObject.append({
+        print(path)
+        dbItem = {
             "path": path,
-            "artist": artist,
-            "title": title,
+            "artist": Path(file).stem,
+            "title": Path(file).stem,
             #"album": album,
-            "year": year,
-            "length": length,
-            "size": size,
-            "bpmdetect": float(detectBpm(file)),
+            "year": 0,
+            "length": 0,
+            "size": 0,
+            "bpmdetect": 0,
             "bpm": 0,
-            "key": str(detectKey(file)),
+            "key": "",
             "downbeat": None,
-            "peakfile": peakJsonFilePath.replace(pathSearchReplace[0], pathSearchReplace[1]),
+            "peakfile": "",
             "hotcues": []
+        }
+        getTagsForFile(dbItem)
 
+        if len(pathSearchReplace) == 2:
+            dbItem["path"] = dbItem["path"].replace(pathSearchReplace[0], pathSearchReplace[1])
 
-        })
+        peakJsonFilePath = ''
+        if createPeakFile == True:
+            md5 = hashlib.md5(path.encode()).hexdigest()
+            peakJsonFileName = Path(path).stem[0:30]
+            peakJsonFilePath = f'{peakfilesDir}/{peakJsonFileName}-{md5}.json'
+            createPeakFile(file, peakJsonFilePath)
+            dbItem["peakfile"] = peakJsonFilePath.replace(pathSearchReplace[0], pathSearchReplace[1])
+
+        dbItem["bpmdetect"] = float(detectBpm(file))
+        dbItem["key"] = str(detectKey(file))
+        jsonObject.append(dbItem)
 
         fileCount += 1
         #if fileCount > 3:
         #    break
-
 
     writeTextFile(
         Path(f'{instrupellaDir}/{targetJson}'),
